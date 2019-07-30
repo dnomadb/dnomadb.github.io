b159d5fe241e14f69e998974315eb0cd817c2d48
dnomadb <damon@mapbox.com>
2019-07-30 15:07:13 -0700

    hash

--- tiltshift-2/bundle.js
+++ tiltshift-2/bundle.js
@@ -6,7 +6,8 @@ const A = new mapboxgl.Map({
     container: 'map-A',
     style: 'mapbox://styles/dnomadb/cju1el5dt14ff1fthtpj0fqli?fresh=true',
     center: [-122.4194, 37.7749],
-    zoom: 10
+    zoom: 10,
+    hash: true
 });
 
 const B = new mapboxgl.Map({
@@ -31,21 +32,6 @@ A.on("load", () => {
 
     mapcont.setAttribute("style", `-webkit-mask-box-image: -webkit-linear-gradient(black ${a}%, transparent ${b}%, transparent ${c}%, black ${d}%)`);
   }, 10);
-
-  // A.on('mousemove', (e) => {
-  //   const offset = 40;
-  //   const y = e.point.y;
-  //   const ctr = (y / height) * 100;
-  //   const a = Math.max(0, ctr - offset);
-  //   const b = Math.max(0, ctr - offset / 2);
-  //   const c = Math.min(height, ctr + offset / 2);
-  //   const d = Math.min(height, ctr + offset);
-  //
-  //   mapcont.setAttribute("style", `-webkit-mask-box-image: -webkit-linear-gradient(black ${a}%, transparent ${b}%, transparent ${c}%, black ${d}%)`);
-  // });
-  // A.on('resize', () => {
-  //   height = A.getContainer().offsetHeight
-  // });
 })
 
 

19d35f47c623c90ede062958662187c0f2562a90
dnomadb <damon@mapbox.com>
2020-10-07 00:59:20 -0700

    support other geometries

--- tilecover/bundle.js
+++ tilecover/bundle.js
@@ -1,80 +1,130 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const cover= require("@mapbox/tile-cover");
-mapboxgl.accessToken = 'pk.eyJ1IjoiZG5vbWFkYiIsImEiOiJjaW16aXFsZzUwNHJmdjdra3h0Nmd2cjY1In0.SqzkaKalXxQaPhQLjodQcQ';
-  const map = new mapboxgl.Map({
-      container: 'map', // container id
-      style: 'mapbox://styles/mapbox/dark-v9', //hosted style id
-      center: [-91.874, 42.76], // starting position
-      zoom: 4, // starting zoom
-      hash: true
-  });
+const cover = require("@mapbox/tile-cover");
+const tilebelt = require("@mapbox/tilebelt");
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZG5vbWFkYiIsImEiOiJjaW16aXFsZzUwNHJmdjdra3h0Nmd2cjY1In0.SqzkaKalXxQaPhQLjodQcQ";
+const map = new mapboxgl.Map({
+  container: "map", // container id
+  style: "mapbox://styles/mapbox/dark-v9", //hosted style id
+  center: [-91.874, 42.76], // starting position
+  zoom: 4, // starting zoom
+  hash: true
+});
 
-  const draw = new MapboxDraw({
-      displayControlsDefault: false,
-      controls: {
-          polygon: true,
-          trash: true
-      }
-  });
-  map.addControl(draw);
-  let bbox = {'type': 'Feature', 'geometry': { 'type': 'Polygon', 'coordinates': []}};
-  let tileCover = {'type': 'Feature', 'geometry': { 'type': 'Polygon', 'coordinates': []}};
-
-  const updateArea = (e) => {
-      const data = draw.getAll();
-      if (data.features.length > 0) {
-          const searchParams = new URLSearchParams(window.location.search);
-          let zoom = searchParams.get("zoom") || (map.getZoom() + 2);
-          zoom = parseInt(zoom);
-          const area = Math.round(turf.area(data) * 1e-4) / 100;
-          bbox = turf.bboxPolygon(turf.bbox(data.features[data.features.length - 1]));
-          const bboxArea = Math.round(turf.area(bbox) * 1e-4) / 100;
-          const tileCover = cover.geojson(data.features[data.features.length - 1].geometry, {min_zoom: zoom,max_zoom: zoom});
-          const tileCoverArea = Math.round(turf.area(tileCover) * 1e-4) / 100;
-          map.getSource('tile-cover').setData(tileCover);
-          map.getSource('bbox').setData(bbox)
-          document.getElementById('poly-area').innerHTML = `<p>Polygon area: <strong>${area} km²</strong></p>`;
-          document.getElementById('bbox-area').innerHTML = `<p>Bbox area: <strong>${bboxArea} km²</strong></p>`;
-          document.getElementById('tilecover-area').innerHTML = `<p>Tilecover area: <strong>${tileCoverArea} km²</strong></p>`;
-      } else {
-        console.log("no features")
-      }
+const draw = new MapboxDraw({
+  displayControlsDefault: false,
+  controls: {
+    polygon: true,
+    line_string: true,
+    point: true,
+    trash: true
   }
-  map.on('draw.create', updateArea);
-  map.on('draw.delete', updateArea);
-  map.on('draw.update', updateArea);
-  map.on('load', () => {
-    map.addSource('bbox', {
-      'type': 'geojson',
-      'data': bbox
-    });
-    map.addLayer({
-      'id': 'bbox',
-        'type': 'line',
-        'source': 'bbox',
-        'paint': {
-          'line-color': '#F28474',
-          'line-opacity': 0.75,
-          'line-width': 5
-        }
-    });
-    map.addSource('tile-cover', {
-      'type': 'geojson',
-      'data': tileCover
-    });
-    map.addLayer({
-      'id': 'tile-cover',
-        'type': 'line',
-        'source': 'tile-cover',
-        'paint': {
-          'line-color': '#DFF16E',
-          'line-opacity': 0.75,
-          'line-width': 5
-        }
-    });
-  })
+});
+map.addControl(draw);
+let bbox = { type: "Feature", geometry: { type: "Polygon", coordinates: [] } };
+let tileCover = {
+  type: "Feature",
+  geometry: { type: "Polygon", coordinates: [] }
+};
 
-},{"@mapbox/tile-cover":2}],2:[function(require,module,exports){
+const makeTileCover = (features, zoom) => {
+  const tiles = [
+    ...new Set(
+      features.reduce((a, f) => {
+        return a.concat(
+          cover.indexes(f.geometry, { min_zoom: zoom, max_zoom: zoom })
+        );
+      }, [])
+    )
+  ];
+
+  return {
+    type: "FeatureCollection",
+    features: tiles.map(t => {
+      return {
+        type: "Feature",
+        properties: {},
+        geometry: tilebelt.tileToGeoJSON(tilebelt.quadkeyToTile(t))
+      };
+    })
+  };
+};
+
+const updateArea = e => {
+  const data = draw.getAll();
+  if (data.features.length > 0) {
+    const searchParams = new URLSearchParams(window.location.search);
+    let zoom = searchParams.get("zoom") || map.getZoom() + 2;
+    zoom = parseInt(zoom);
+    const area = Math.round(turf.area(data) * 1e-4) / 100;
+    bbox = turf.bboxPolygon(turf.bbox(data));
+    const bboxArea = Math.round(turf.area(bbox) * 1e-4) / 100;
+    const tileCover = makeTileCover(data.features, zoom);
+    const tileCoverArea = Math.round(turf.area(tileCover) * 1e-4) / 100;
+    map.getSource("tile-cover").setData(tileCover);
+    map.getSource("bbox").setData(bbox);
+    document.getElementById(
+      "poly-area"
+    ).innerHTML = `<p>Polygon area: <strong>${area} km²</strong></p>`;
+    document.getElementById(
+      "bbox-area"
+    ).innerHTML = `<p>Bbox area: <strong>${bboxArea} km²</strong></p>`;
+    document.getElementById(
+      "tilecover-area"
+    ).innerHTML = `<p>Tilecover area: <strong>${tileCoverArea} km²</strong></p>`;
+  } else {
+    map
+      .getSource("tile-cover")
+      .setData({
+        type: "Feature",
+        geometry: { type: "Polygon", coordinates: [] }
+      });
+    map
+      .getSource("bbox")
+      .setData({
+        type: "Feature",
+        geometry: { type: "Polygon", coordinates: [] }
+      });
+    document.getElementById("poly-area").innerHTML = "";
+    document.getElementById("bbox-area").innerHTML = "";
+    document.getElementById("tilecover-area").innerHTML = "";
+  }
+};
+map.on("draw.create", updateArea);
+map.on("draw.delete", updateArea);
+map.on("draw.update", updateArea);
+map.on("load", () => {
+  map.addSource("bbox", {
+    type: "geojson",
+    data: bbox
+  });
+  map.addLayer({
+    id: "bbox",
+    type: "line",
+    source: "bbox",
+    paint: {
+      "line-color": "#F28474",
+      "line-opacity": 0.75,
+      "line-width": 5
+    }
+  });
+  map.addSource("tile-cover", {
+    type: "geojson",
+    data: tileCover
+  });
+  map.addLayer({
+    id: "tile-cover",
+    type: "line",
+    source: "tile-cover",
+    paint: {
+      "line-color": "#DFF16E",
+      "line-opacity": 0.75,
+      "line-width": 5
+    }
+  });
+});
+
+},{"@mapbox/tile-cover":2,"@mapbox/tilebelt":3}],2:[function(require,module,exports){
 'use strict';
 
 var tilebelt = require('@mapbox/tilebelt');

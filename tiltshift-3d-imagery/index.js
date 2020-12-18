07d352662260b1caa361ba3072d96de43bbd1b91
dnomadb <damon@mapbox.com>
2020-12-18 12:14:03 -0800

    hi

---
+++ tiltshift-3d-imagery/index.js
@@ -0,0 +1,65 @@
+const syncMove = require('@mapbox/mapbox-gl-sync-move');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZG5vbWFkYiIsImEiOiJjaW16aXFsZzUwNHJmdjdra3h0Nmd2cjY1In0.SqzkaKalXxQaPhQLjodQcQ';
+const A = new mapboxgl.Map({
+    container: 'map-A',
+    style: 'mapbox://styles/mapbox/satellite-v9',
+    center: [-120, 40],
+    zoom: 9,
+    hash: true
+});
+
+A.on("load", () => {
+  A.addSource("mapbox-dem", {
+    type: "raster-dem",
+    url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+    tileSize: 512,
+    maxzoom: 14
+  });
+  // add the DEM source as a terrain layer with exaggerated height
+  A.setTerrain({
+    source: "mapbox-dem",
+    exaggeration: [
+      "interpolate",
+      ["linear", 0.5],
+      ["zoom"],
+      0,
+      4,
+      16,
+      1
+    ]
+  });
+});
+
+
+const B = new mapboxgl.Map({
+    container: 'map-B',
+    style: 'mapbox://styles/mapbox/satellite-v9',
+    center: [-120, 40],
+    zoom: 9,
+    hash: true
+});
+
+B.on("load", () => {
+  B.addSource("mapbox-dem", {
+    type: "raster-dem",
+    url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+    tileSize: 512,
+    maxzoom: 14
+  });
+  // add the DEM source as a terrain layer with exaggerated height
+  B.setTerrain({
+    source: "mapbox-dem",
+    exaggeration: [
+      "interpolate",
+      ["linear", 0.5],
+      ["zoom"],
+      0,
+      4,
+      16,
+      1
+    ]
+  });
+});
+
+syncMove(A, B);

88267ceed1772c42694efac1de4b01e0a8cd9813
dnomadb <damon@mapbox.com>
2021-01-26 23:46:22 -0800

    allow order changing

--- reflect/index.js
+++ reflect/index.js
@@ -1,4 +1,12 @@
 const syncMove = require('@mapbox/mapbox-gl-sync-move');
+const query = new URLSearchParams(window.location.search);
+const order = query.get("order") || "cave";
+let top, bottom
+if (order == "cave") {
+  document.getElementById("map-A").classList.add("flip");
+} else {
+  document.getElementById("map-B").classList.add("flip");
+}
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZG5vbWFkYiIsImEiOiJjaW16aXFsZzUwNHJmdjdra3h0Nmd2cjY1In0.SqzkaKalXxQaPhQLjodQcQ';
 const A = new mapboxgl.Map({

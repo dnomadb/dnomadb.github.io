435b06c6ccd091027771fb115d5a9770e7ba3e35
Damon <nomad.damon@gmail.com>
2022-06-29 16:26:11 -0700

    add package

--- make-a-constellation/src/index.js
+++ make-a-constellation/src/index.js
@@ -32,21 +32,6 @@ const draw = new MapboxDraw({
 
 map.addControl(draw, "top-right");
 
-const toHexString = (byteArray) => {
-  return Array.prototype.map
-    .call(byteArray, function (byte) {
-      return ("0" + (byte & 0xff).toString(16)).slice(-2);
-    })
-    .join("");
-};
-const toByteArray = (hexString) => {
-  var result = [];
-  for (var i = 0; i < hexString.length; i += 2) {
-    result.push(parseInt(hexString.substr(i, 2), 16));
-  }
-  return result;
-};
-
 map.on("load", () => {
   const constellationString = url.searchParams.get("constellation");
   if (constellationString) {

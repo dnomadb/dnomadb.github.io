1e62654814d119556691feb932d7f95f55719d2e
Damon <nomad.damon@gmail.com>
2022-09-30 14:38:47 -0700

    omg

---
+++ OMG/bundle.js
@@ -0,0 +1,160 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+const syncMove = require("@mapbox/mapbox-gl-sync-move");
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZG5vbWFkYiIsImEiOiJjaW16aXFsZzUwNHJmdjdra3h0Nmd2cjY1In0.SqzkaKalXxQaPhQLjodQcQ";
+const A = new mapboxgl.Map({
+  container: "map-A",
+  style: {
+    version: 8,
+    sources: {
+      "raster-tiles": {
+        type: "raster",
+        url: "mapbox://mapbox.satellite",
+        tileSize: 256,
+      },
+    },
+    layers: [
+      {
+        id: "simple-tiles",
+        type: "raster",
+        source: "raster-tiles",
+        minzoom: 0,
+        maxzoom: 22,
+      },
+    ],
+  },
+  center: [-120, 40],
+  zoom: 9,
+  hash: true,
+});
+
+A.on("load", () => {
+  A.addSource("mapbox-dem", {
+    type: "raster-dem",
+    url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+    tileSize: 512,
+    maxzoom: 14,
+  });
+  // add the DEM source as a terrain layer with exaggerated height
+  A.setTerrain({
+    source: "mapbox-dem",
+    exaggeration: ["interpolate", ["linear", 0.5], ["zoom"], 0, 10, 16, 10],
+  });
+});
+
+const B = new mapboxgl.Map({
+  container: "map-B",
+  style: {
+    version: 8,
+    sources: {
+      "raster-tiles": {
+        type: "raster",
+        url: "mapbox://mapbox.satellite",
+        tileSize: 4096,
+      },
+    },
+    layers: [
+      {
+        id: "simple-tiles",
+        type: "raster",
+        source: "raster-tiles",
+        minzoom: 0,
+        maxzoom: 22,
+        paint: { "raster-resampling": "nearest" },
+      },
+    ],
+  },
+  center: [-120, 40],
+  zoom: 9,
+  hash: true,
+});
+
+B.on("load", () => {
+  B.addSource("mapbox-dem", {
+    type: "raster-dem",
+    url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+    tileSize: 512,
+    maxzoom: 14,
+  });
+  // add the DEM source as a terrain layer with exaggerated height
+  B.setTerrain({
+    source: "mapbox-dem",
+    exaggeration: ["interpolate", ["linear", 0.5], ["zoom"], 0, 10, 16, 10],
+  });
+});
+
+syncMove(A, B);
+
+},{"@mapbox/mapbox-gl-sync-move":2}],2:[function(require,module,exports){
+function moveToMapPosition (master, clones) {
+  var center = master.getCenter();
+  var zoom = master.getZoom();
+  var bearing = master.getBearing();
+  var pitch = master.getPitch();
+
+  clones.forEach(function (clone) {
+    clone.jumpTo({
+      center: center,
+      zoom: zoom,
+      bearing: bearing,
+      pitch: pitch
+    });
+  });
+}
+
+// Sync movements of two maps.
+//
+// All interactions that result in movement end up firing
+// a "move" event. The trick here, though, is to
+// ensure that movements don't cycle from one map
+// to the other and back again, because such a cycle
+// - could cause an infinite loop
+// - prematurely halts prolonged movements like
+//   double-click zooming, box-zooming, and flying
+function syncMaps () {
+  var maps;
+  var argLen = arguments.length;
+  if (argLen === 1) {
+    maps = arguments[0];
+  } else {
+    maps = [];
+    for (var i = 0; i < argLen; i++) {
+      maps.push(arguments[i]);
+    }
+  }
+
+  // Create all the movement functions, because if they're created every time
+  // they wouldn't be the same and couldn't be removed.
+  var fns = [];
+  maps.forEach(function (map, index) {
+    fns[index] = sync.bind(null, map, maps.filter(function (o, i) { return i !== index; }));
+  });
+
+  function on () {
+    maps.forEach(function (map, index) {
+      map.on('move', fns[index]);
+    });
+  }
+
+  function off () {
+    maps.forEach(function (map, index) {
+      map.off('move', fns[index]);
+    });
+  }
+
+  // When one map moves, we turn off the movement listeners
+  // on all the maps, move it, then turn the listeners on again
+  function sync (master, clones) {
+    off();
+    moveToMapPosition(master, clones);
+    on();
+  }
+
+  on();
+  return function(){  off(); fns = []; maps = []; };
+}
+
+module.exports = syncMaps;
+
+},{}]},{},[1]);

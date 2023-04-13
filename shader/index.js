e58a757acbea734dd2178c86af26d92c2e595393
Damon <nomad.damon@gmail.com>
2023-04-13 10:08:10 -0700

    slr

--- shader/index.js
+++ shader/index.js
@@ -18,40 +18,56 @@ var fragmentSource = `
     precision mediump float;
     varying vec2 vTexCoord;
     uniform sampler2D uTexture;
-    // uniform elevation
+    uniform float u_slr;
 
     void main() {
-        float elevationScale = 15.0;
         float interval = 0.05;
         vec4 color = texture2D(uTexture, vTexCoord);
-        float i = -10000.0 + (color.r * 255.0 * 256.0 + color.g * 255.0 + color.b) * 0.1;
-        float e = (mod(i, interval) - ((mod(floor(i / interval), 2.0)) * interval)) * (mod(floor(i / interval), -2.0) * 2.0 + 1.0);
-        gl_FragColor = vec4(0, 1.0 - e * elevationScale, 1.0 - e * elevationScale, 1);
+        float i = -10000.0 + (color.r * 255.0 * 256.0 * 256.0 + color.g * 256.0 * 255.0 + color.b * 256.0) * 0.1;
+        if (i <= u_slr) {
+          gl_FragColor = vec4(0.15, 0.5, 0.75, 0.75);
+        }
+        else {
+          gl_FragColor = vec4(0, 0, 0, 0);
+        }
     }           
      `;
 const map = new mapboxgl.Map({
-  container: document.getElementById("map"),
-  style: "mapbox://styles/mapbox/empty-v8",
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
   center: [145, -16],
   zoom: 0,
   hash: true,
+  tilt: false,
+  // transformRequest: (r, t) => {
+  //   return {url: r.replace("\@2x", "")}
+  // }
 });
+const customlayer = new TextureLayer(
+  "test",
+  {
+    type: "raster",
+    url: "mapbox://mapbox.terrain-rgb"
+  },
+  setupLayer,
+  render
+);
 map.on("load", () => {
-  const customlayer = new TextureLayer(
-    "test",
-    {
-      type: "raster",
-    //   tiles: ["http://localhost:3000/tiles/rgb/{z}/{x}/{y}.png"]
-      url: "mapbox://mapbox.mapbox-terrain-dem-v1",
-    },
-    setupLayer,
-    render
-  );
-  map.addLayer(customlayer);
+  map.addLayer(customlayer, "aerialway");
+  map.getStyle().layers.forEach(l => {
+    if (l["source-layer"] == 'water') console.log(l)
+  })
 });
 
+
+
 let program;
+function logGLCall(functionName, args) {   
+  console.log("gl." + functionName + "(" + 
+     WebGLDebugUtils.glFunctionArgsToString(functionName, args) + ")");   
+}
 function setupLayer(map, gl) {
+  gl = WebGLDebugUtils.makeDebugContext(gl, undefined, logGLCall);
   const vertexShader = gl.createShader(gl.VERTEX_SHADER);
   gl.shaderSource(vertexShader, vertexSource);
   gl.compileShader(vertexShader);
@@ -61,6 +77,7 @@ function setupLayer(map, gl) {
   gl.compileShader(fragmentShader);
 
   program = gl.createProgram();
+
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
   gl.linkProgram(program);
@@ -69,33 +86,47 @@ function setupLayer(map, gl) {
   program.aPos = gl.getAttribLocation(program, "aPos");
   program.uMatrix = gl.getUniformLocation(program, "uMatrix");
   program.uTexture = gl.getUniformLocation(program, "uTexture");
+  program.u_slr = gl.getUniformLocation(program, "u_slr");
   const vertexArray = new Float32Array([0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1]);
 
   program.vertexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, program.vertexBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, vertexArray, gl.STATIC_DRAW);
 }
+let i = 10;
 function render(gl, matrix, tiles) {
-  gl.useProgram(program);
-
-  tiles.forEach((tile) => {
-    if (!tile.texture) return;
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, tile.texture.texture);
-
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, program.vertexBuffer);
-    gl.enableVertexAttribArray(program.a_pos);
-    gl.vertexAttribPointer(program.aPos, 2, gl.FLOAT, false, 0, 0);
-    gl.uniformMatrix4fv(program.uMatrix, false, tile.tileID.projMatrix);
-    gl.uniform1i(program.uTexture, 0);
-    gl.depthFunc(gl.LESS);
-    gl.enable(gl.BLEND);
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
-  });
+  const renderTiles = () => {
+    gl.useProgram(program);
+    tiles.forEach((tile) => {
+      if (!tile.texture) return;
+      gl.activeTexture(gl.TEXTURE0);
+      gl.bindTexture(gl.TEXTURE_2D, tile.texture.texture);
+  
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  
+      gl.bindBuffer(gl.ARRAY_BUFFER, program.vertexBuffer);
+      gl.enableVertexAttribArray(program.a_pos);
+      gl.vertexAttribPointer(program.aPos, 2, gl.FLOAT, false, 0, 0);
+      gl.uniformMatrix4fv(program.uMatrix, false, tile.tileID.projMatrix);
+      gl.uniform1i(program.uTexture, 0);
+      gl.uniform1f(program.u_slr, i);
+      gl.depthFunc(gl.LESS);
+      gl.enable(gl.BLEND);
+      gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+      gl.drawArrays(gl.TRIANGLES, 0, 6);
+    });
+  }
+  renderTiles()
+}
+const slider = document.getElementById("slr")
+console.dir(slider, {depth: null})
+slider.oninput = function() {
+  i = slider.value;
+  // oninput="rangeValue.innerText = this.value"
+  document.getElementById("rangeValue").innerText = this.value;
+  customlayer.updateTiles()
+  map.triggerRepaint()
 }
